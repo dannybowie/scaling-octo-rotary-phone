11c7dd0aa4cacb4940710e7a788c05c535fcabe0
danny <kremzan17@gmail.com>
2026-01-07 23:38:31 -0500

    version 2 updates

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,32 +1,51 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+//import { RouterLink } from '@angular/router';
 import { NgForm, FormsModule } from '@angular/forms';
 import { Firestore, collection, addDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink, FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
   private firestore = inject(Firestore);
+  submissionSuccess = false;
 
 onSubmit(form: NgForm) {
   if (form.valid) {
-    const { fName, lName, email, religion } = form.value;
+    const { fName, lName, email, phone, dob, religion, church, baptized } = form.value;
       
       const usersCollection = collection(this.firestore, 'registrations');
       interface UserData {
         fName: string;
         lName: string;
         email: string;
+        phone: string;
+        dob: Date;
+        church: string;
+        baptized: string;
         religion: string;
       }
 
-      addDoc(usersCollection, { fName, lName, email, religion } as UserData).then(() => {
+      addDoc(usersCollection, { 
+        fName, 
+        lName, 
+        email, 
+        phone, 
+        dob: new Date(dob), 
+        religion, 
+        church, 
+        baptized 
+      } as UserData).then(() => {
         console.log('Form Submitted to Firebase');
+        this.submissionSuccess = true;
+        setTimeout(() => {
+          this.submissionSuccess = false;
+        } , 5000)
         form.resetForm();
       }).catch((error: Error) => {
         console.error('Error adding document: ', error);
